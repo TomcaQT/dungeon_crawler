7e984033956ebfec6137be91e6398648d9f13dcb
Tomáš Valenta <TomiukQT@gmail.com>
2022-01-02 11:32:57 +0100

    Add color utils

---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+
+    public static Color ColorFromInt(int r, int g, int b, float a = 1) 
+        => new Color(r / 255f, g / 255f, b / 255f, a);
+    
+
+    public static Color ItemQualityToColor(ItemQuality itemQuality)
+    {
+        switch (itemQuality)
+        {
+            case ItemQuality.Normal:
+                return Color.green;
+            case ItemQuality.Rare:
+                return Color.blue;
+            case ItemQuality.Epic:
+                return ColorFromInt(157, 71, 237);
+            case ItemQuality.Legendary:
+                return ColorFromInt(237, 160, 71);
+            default:
+                return Color.white;
+        }
+    }
+}
