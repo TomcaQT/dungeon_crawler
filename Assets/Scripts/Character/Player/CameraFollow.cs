c786bbaef27055020fb2fdd9a50870d2b05c5b30
Tomáš Valenta <TomiukQT@gmail.com>
2022-01-05 21:27:38 +0100

    Update CameraFollow.cs

--- Assets/Scripts/Character/Player/CameraFollow.cs
+++ Assets/Scripts/Character/Player/CameraFollow.cs
@@ -3,13 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
     [SerializeField] private float _smoothSpeed = 0.125f;
+    [SerializeField] private float _size = 8f;
     [SerializeField] private Vector3 _offset;
 
+    private Camera _camera;
+    
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+            OnValidate();
+    }
+
+    private void OnValidate()
+    {
+        if (_camera != null) 
+            _camera.orthographicSize = _size;
+    }
+    
     private void FixedUpdate()
     {
         Vector3 desiredPosition = _target.position + _offset;
