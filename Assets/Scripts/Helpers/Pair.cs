94e7d625b14ec96411c7dc84bbde942d38fce278
Tomáš Valenta <TomiukQT@gmail.com>
2022-01-05 20:26:00 +0100

    Serializable pair

---
+++ Assets/Scripts/Helpers/Pair.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Helpers
+{
+    /// <summary>
+    /// Container as nearly same as Tuple<T1,T2> (C++ pair-like) but can be serialized in Inspector
+    /// </summary>
+    /// <typeparam name="T1">Type of first value</typeparam>
+    /// <typeparam name="T2">Type of second value</typeparam>
+    [System.Serializable]
+    public struct Pair<T1,T2>
+    {
+        private T1 _first;
+        private T2 _second;
+        
+        public Pair(T1 first, T2 second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        //Properties
+        public T1 First => _first;
+        public T2 Second => _second;
+
+    }
+}
