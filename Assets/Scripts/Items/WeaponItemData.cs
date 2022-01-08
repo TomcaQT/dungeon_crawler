d53fe812af34aeb988995b438ef4917c37748503
Tomáš Valenta <TomiukQT@gmail.com>
2022-01-08 10:02:31 +0100

    Weapons

---
+++ Assets/Scripts/Items/WeaponItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Items;
+using UnityEngine;
+
+public class WeaponItemData : ItemData
+{
+    public override void OnPickup(PlayerStats player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
